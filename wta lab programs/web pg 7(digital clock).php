c0a0bd949fa6d5b6328bbb5be99051ce691739ba
Nehacnaik <55840972+Nehacnaik@users.noreply.github.com>
2021-07-26 17:22:17 +0530

    Add files via upload

---
+++ wta lab programs/web pg 7(digital clock).php
@@ -0,0 +1,4 @@
+<head>
+ <meta http-equiv="refresh" content="1"/>
+ <p> <?php echo date(" h: i : s A");?> </p>
+</head>
---
+++ wta lab programs/web pg10(student table).sql
@@ -0,0 +1,12 @@
+CREATE TABLE `student` (
+  `usn` varchar(20),
+  `name` varchar(20),
+  `marks` int(11)
+);
+
+INSERT INTO `student` (`usn`, `name`, `marks`) VALUES
+('3GN16cs044', 'Sam', 67),
+('3GN16cs030', 'Sanvi', 87),
+('3GN16cs002', 'Mili', 97),
+('3GN16cs014', 'Prerana', 77),
+('3GN16cs023', 'Pranav', 57);
